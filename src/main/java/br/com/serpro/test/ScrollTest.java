2c9298c8817b001df89c4bf65ae57c7695a761f2
Andre <andreviveiros@hotmail.com>
2019-06-06 13:34:37 -0300

    Test Scroll porém ainda não funciona!

---
+++ src/main/java/br/com/serpro/test/ScrollTest.java
@@ -0,0 +1,22 @@
+package br.com.serpro.test;
+
+import org.junit.Test;
+
+import br.com.serpro.appiumCore.BaseTest;
+import br.com.serpro.pages.MenuPage;
+
+public class ScrollTest extends BaseTest {
+	
+	MenuPage menuPage = new MenuPage();
+	
+	@Test
+	public void escolherOpcaoEscondida() {
+		
+//		menuPage.scroll(0.8, 0.1);
+		menuPage.scrollA(0.8, 0.1);
+		esperar(1000);
+		menuPage.clicarPorTexto("Opção bem escondida");
+//		menuPage.existeElementoPorTexto("Você achou essa opção");
+//		menuPage.clicarPorTexto("OK");
+	}
+}
